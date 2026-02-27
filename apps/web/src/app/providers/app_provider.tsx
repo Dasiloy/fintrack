085f40a1ddddc569c70fb70521877bc2c095e763
dasiloy <oyewoledamilare00@gmail.com>
2026-02-27 14:42:49 +0100

    feat: MailTrap
     Integrate Mailtrap for email handling and add Vercel Speed Insights for performance monitoring; update email templates for improved styling and structure

--- apps/web/src/app/providers/app_provider.tsx
+++ apps/web/src/app/providers/app_provider.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { AppProgressProvider } from '@bprogress/next';
 import { TooltipProvider, AlertProvider } from '@ui/components';
 
 export default function AppProviver({ children }: React.PropsWithChildren) {
   return (
     <AlertProvider>
-      <TooltipProvider>{children}</TooltipProvider>
+      <TooltipProvider>
+        <AppProgressProvider
+          color="#7c7aff"
+          disableSameURL
+          shallowRouting={false}
+          shouldCompareComplexProps
+        >
+          {children}
+        </AppProgressProvider>
+      </TooltipProvider>
     </AlertProvider>
   );
 }
