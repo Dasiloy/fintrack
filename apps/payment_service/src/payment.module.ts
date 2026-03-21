c32bb88d38f2da19a2e3e20bccc3a358f1f339e5
dasiloy <oyewoledamilare00@gmail.com>
2026-03-21 14:42:22 +0100

    chore: Update environment variables and enhance payment module configuration
    - Added STRIPE_WEBHOOK_SECRET and MICROSERVICE_NAME environment variables to relevant modules for improved configuration.
    - Enhanced the payment module to validate new environment variables using Joi.
    - Updated package.json in the payment service to include the Stripe package for payment processing.
    - Improved the account statement export component with new download options for CSV and PDF formats.
    - Refactored the ProfileSection component to conditionally display the save button based on the showSave prop.

--- apps/payment_service/src/payment.module.ts
+++ apps/payment_service/src/payment.module.ts
@@ -1,6 +1,11 @@
+import * as Joi from 'joi';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 
+import { LoggerModule } from '@fintrack/common/logger/logger.module';
+import { DatabaseModule } from '@fintrack/database/nest';
 import { RpcAuthGuard } from '@fintrack/common/guards/rpc.guard';
 import { GrpcLoggingInterceptor } from '@fintrack/common/logger/grpc-logging.interceptor';
 
@@ -8,7 +13,25 @@ import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: `.env`,
+      expandVariables: true,
+      validationSchema: Joi.object({
+        REDIS_URL: Joi.string().required(),
+        DATABASE_URL: Joi.string().required(),
+        DATABASE_CA_CERTIFICATE: Joi.string().required(),
+        MICROSERVICE_NAME: Joi.string().required(),
+        STRIPE_WEBHOOK_SECRET: Joi.string().required(),
+        STRIPE_SECRET_KEY: Joi.string().required(),
+        STRIPE_PRO_MONTHLY_PRICE_ID: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    LoggerModule,
+  ],
   controllers: [PaymentController],
   providers: [
     PaymentService,
--- apps/web/src/app/(dashboard)/settings/account/_components/export_account_statement.tsx
+++ apps/web/src/app/(dashboard)/settings/account/_components/export_account_statement.tsx
@@ -3,16 +3,35 @@
 import Image from 'next/image';
 import { api_client } from '@/lib/trpc_app/api_client';
 import { ProfileSection } from '@/app/(dashboard)/settings/account/_components/profile_section';
+import { Button } from '@ui/components';
+import { Download, File } from 'lucide-react';
 
 export function ExportAccountStatement() {
   const getMe = api_client.user.getMe.useQuery();
   return (
     <ProfileSection
       title="Data & Privacy"
+      showSave={false}
       description="Export your account statement"
       Icon={<Image width={20} height={20} alt="Export Account Statement" src="/shield.svg" />}
     >
-      <div className="grid grid-cols-1 gap-4"></div>
+      <div className="bg-bg-surface rounded-xl p-4">
+        <div className="flex flex-col gap-0.5">
+          <p className="text-text-primary text-body font-medium">Export Account Statement</p>
+          <p className="text-text-tertiary text-body-sm font-normal">
+            Download all your transaction and budget history
+          </p>
+        </div>
+
+        <div className="mt-4 flex items-center gap-2">
+          <Button size="default" variant={'outline'}>
+            <Download /> CSV
+          </Button>
+          <Button size="default" variant={'outline'}>
+            <File /> PDF
+          </Button>
+        </div>
+      </div>
     </ProfileSection>
   );
 }
